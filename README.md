953bd147c9f503df606eedae30c2ad17e3176aea
Pablo <pablopenyamulero@gmail.com>
2023-02-08 09:39:45 +0100

    actualización del README day One

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-startr here
+Day One
+
+Martes 07, Febrero 2023
+
+El día de hoy, después de entender el código, he completado la tarea de conectar el proyecto al repositorio de Github y a la base de datos en Supabase, la cual he conseguido que me guarde los datos de los usuarios. Lo que no he podido conseguir es que me compruebe si el usuario y la contraseña son correctos, y este será mi objetivo para mañana miércoles.
+
+[x] Conectar el proyecto al repositorio de Github.
+[x] Conectar el proyecto a la base de datos de Supabase.
+[ ] Hacer la comprobación de usuario y contraseña para acceder al home.
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,17 +1,32 @@
 <!-- COMPONENTE BOILERPLATE -->
- 
-  <template>
 
+<template>
   <div class="container">
-    <h3 class="header-title">Log In to ToDo App</h3>
-    <p class="header-subtitle">Estamos en la ruta de login. Aquí deberíais crear un form con la lógica correspondiente para que este permita al usuario loguearse con su email y su contraseña. Miraros la lógica de SignUp si necesitáis inspiración :)</p>
-    <p>Dont have an account? <PersonalRouter :route="route" :buttonText="buttonText" class="sign-up-link"/></p>
-  </div>
+    <h3 class="header-title">Welcome to Wu Tasks</h3>
+    <p class="header-subtitle">Start Organizing your tasks todays!</p>
+    <p>Sing in</p>
+    <form action="singIn">
+      <input type="email" placeholder="example@gmail.com" v-model="email" />
+      <input type="pasword" placeholder="**********" v-model="pasword" />
+      <button class="button" type="submit">Sign In</button>
+    </form>
 
+    <p>
+      Dont have an account?
+      <PersonalRouter
+        :route="route"
+        :buttonText="buttonText"
+        class="sign-up-link"
+      />
+    </p>
+  </div>
 </template>
 
 <script setup>
 import PersonalRouter from "./PersonalRouter.vue";
+import { ref, reactive }
+
+
 
 // Route Variables
 const route = "/auth/signup";
@@ -19,7 +34,16 @@ const buttonText = "Sign Up";
 
 // Arrow function to Signin user to supaBase
 const signIn = async () => {
-  try {} catch (error) {}
+  console.log("Hola pepsicola");
+  if (email.value) {
+    try {
+      await useUserStore().signIn(email.value, password.value);
+      redirect.push({ path: "/" });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 };
 </script>
 
