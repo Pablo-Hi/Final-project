2b03738e5c4c17adcbf98ea87bed8daabef0037d
Pablo <pablopenyamulero@gmail.com>
2023-02-14 13:19:19 +0100

    Sign in page done

--- src/components/ButtonTwo.vue
+++ src/components/ButtonTwo.vue
@@ -5,14 +5,14 @@
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap");
 
-.principal-button {
+.button-two {
   font-size: larger;
   text-decoration: none;
   font-style: normal;
   font-family: "Roboto Slab", serif;
-  background: var(--colorRed);
-  color: var(--colorWhite);
-  border: 2px solid var(--colorBlack);
+  background: var(--colorWhite);
+  color: var(--colorRed);
+  border: 2px solid var(--colorRed);
   padding: 16px 20px;
   border-radius: 3px;
   position: relative;
@@ -21,14 +21,14 @@
   display: inline-block;
   box-shadow: 7px 7px 15px var(--colorBlack);
 }
-.principal-button:hover {
-  color: var(--colorRed);
-  border: 2px solid var(--colorRed);
+.button-two:hover {
+  color: var(--colorWhite);
+  border: 2px solid var(--colorBlack);
 }
 
-.principal-button::after {
+.button-two::after {
   content: "";
-  background: var(--colorWhite);
+  background: var(--colorRed);
   position: absolute;
   z-index: -1;
   padding: 16px 20px;
@@ -40,7 +40,7 @@
   -webkit-transition: all 0.35s;
   transition: all 0.35s;
 }
-.principal-button:hover::after {
+.button-two:hover::after {
   left: 0;
   right: 0;
   top: 0;
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,32 +1,44 @@
 <!-- COMPONENTE BOILERPLATE -->
 
 <template>
-  <div class="containerSingIn">
-    <div>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Alabama_Crimson_Tide_logo.svg/2048px-Alabama_Crimson_Tide_logo.svg.png"
-        alt="alabama logo"
-      />
+  <body class="sign-in-body">
+    <div class="containerSingIn">
+      <div class="alabama-logo">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Alabama_Crimson_Tide_logo.svg/2048px-Alabama_Crimson_Tide_logo.svg.png"
+          alt="alabama logo"
+        />
+      </div>
+
+      <h1 class="header-title">Welcome Player!</h1>
+      <h3 class="header-subtitle">Start Organizing your tasks todays!</h3>
+      <p>Sing in</p>
+      <form class="sign-in-form" @submit.prevent="signIn">
+        <input
+          class="email-input"
+          type="email"
+          placeholder="example@gmail.com"
+          v-model="email"
+        />
+        <input
+          class="email-input"
+          type="password"
+          placeholder="**********"
+          v-model="password"
+        />
+        <GeneralButton type="submit">Sign In</GeneralButton>
+      </form>
+
+      <p class="sign-up-container">
+        Become a Player!
+        <PersonalRouter
+          :route="route"
+          :buttonText="buttonText"
+          class="button-two"
+        />
+      </p>
     </div>
-
-    <h3 class="header-title">Welcome Player!</h3>
-    <p class="header-subtitle">Start Organizing your tasks todays!</p>
-    <p>Sing in</p>
-    <form @submit.prevent="signIn">
-      <input type="email" placeholder="example@gmail.com" v-model="email" />
-      <input type="password" placeholder="**********" v-model="password" />
-      <GeneralButton type="submit">Sign In</GeneralButton>
-    </form>
-
-    <p>
-      Dont have an account?
-      <PersonalRouter
-        :route="route"
-        :buttonText="buttonText"
-        class="principal-button"
-      />
-    </p>
-  </div>
+  </body>
 </template>
 
 <script setup>
@@ -35,6 +47,7 @@ import { ref, reactive } from "vue";
 import { useUserStore } from "../stores/user";
 import { useRouter } from "vue-router";
 import GeneralButton from "./GeneralButton.vue";
+import ButtonTwo from "./ButtonTwo.vue";
 
 // Route Variables
 const route = "/auth/signup";
@@ -61,9 +74,51 @@ const signIn = async () => {
 </script>
 
 <style>
+.sign-in-body {
+  display: flex;
+  justify-content: center;
+  background-image: url("https://cdn.vox-cdn.com/thumbor/E0EL7AhJOZx7EorqdS9-J3PnajA=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/24325828/usa_today_17435839.jpg");
+  background-size: cover;
+  background-position: center;
+}
 .containerSingIn {
+  color: var(--colorBlack);
   display: flex;
   flex-direction: column;
   text-align: center;
+  align-items: center;
+  width: 30vw;
+  margin: 2%;
+  padding: 5%;
+  background-color: var(--colorWhite);
+  border: 2px solid var(--colorBlack);
+  border-radius: 3px;
+  box-shadow: 7px 7px 15px var(--colorBlack);
+  background-image: url("https://media.istockphoto.com/id/1291938358/es/v%C3%ADdeo/humo-llenando-el-fondo-de-movimiento-de-la-pantalla-alfa-mate.jpg?s=640x640&k=20&c=E_aOY2Jobf6QYj1whzJcB4F7INB3HyqKztzSscjiaow=");
+  background-size: cover;
+  background-position: center;
+}
+
+.sign-in-form {
+  display: flex;
+  flex-direction: column;
+  /* height: 20vh; */
+  justify-content: space-evenly;
+}
+
+.email-input {
+  height: 3vw;
+  width: 17vw;
+  border-radius: 3px;
+  box-shadow: 7px 7px 15px var(--colorBlack);
+  margin-bottom: 4px;
+}
+
+.sign-up-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 15vw;
+  width: 17vw;
 }
 </style>
